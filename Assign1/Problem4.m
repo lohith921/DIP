3ae34defa12c4ae3667c27c1fd85d1f027a04c71
lohith921 <nanuvala.lohith@gmail.com>
2015-10-24 12:00:55 -0500

    Final touchups

--- Assign1/Problem4.m
+++ Assign1/Problem4.m
@@ -1,21 +1,21 @@
 % Matlab code for problem 4-Laplacian Enhancement
-% defining the simple laplacial filter
+% defining the simple laplacian filter
 lap1 = [0 1 0; 1 -4 1; 0 1 0];
-% laplacian filter involving the diagonal pixels 
+% laplacian filter involving the diagonal pixels as well
 lap2 = [1 1 1; 1 -8 1; 1 1 1];
 % lap2 with negation
 lap3 = [-1 -1 -1; -1 8 -1; -1 -1 -1];
-I=imread('Uexposed1.jpg');
-I=rgb2gray(I);
-I1=imfilter(I,lap1);
-I2=imfilter(I,lap2);
+Ue = imread('Uexposed1.jpg');
+I = rgb2gray(Ue);
+I1 = imfilter(I,lap1);
+I2 = imfilter(I,lap2);
 %I3 = imfilter(I,lap3);
 figure, imshow(I); title('Uexposed Original');
 figure, imshow(I1); title('Uexposed filtered using Laplacian');
 figure, imshow(I2); title('Uexposed filtered using Laplacian diagonal');
 %figure, imshow(I3); title('Uexposed filtered using Laplacian diagonal inverse');
-A = imread('Oexposed.jpg');
-A = rgb2gray(A);
+Oe = imread('Oexposed.jpg');
+A = rgb2gray(Oe);
 A1 = imfilter(A,lap1);
 A2 = imfilter(A,lap2);
 figure, imshow(A); title('Over exposed Original');
