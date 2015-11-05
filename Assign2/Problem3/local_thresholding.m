d0c5e1bee3b512113d61cf01b107578cc95e2d44
lohith921 <nanuvala.lohith@gmail.com>
2015-11-04 21:14:44 -0600

    local thresholding being modified to suit local window processing

--- Assign2/Problem3/local_thresholding.m
+++ Assign2/Problem3/local_thresholding.m
@@ -3,29 +3,37 @@ function [ I ] = local_thresholding( ip )
 % based on the std and mean in the neighbourhood 
 %ip = im2double(ip);
 [a,b]=size(ip);
-a 
-b
+%fprintf('The sizes are %d, %d\n',a,b);
 % lets compute the mean
 mmean = sum(ip(1:end,1:end));
- % mmean is a column vector with mean for each row of ip
+ % mmean is a column vector with sum for each row of ip
 mmean1 = sum(mmean);
+fprintf('mean sum is %f\n',mmean1);
 p = a*b;
 mmean1 = mmean1/(p);
 mmean1
 sqsum = zeros(1);
+%sqsum
+%sqsum = 0;
 x = zeros(1);
+%x
+%x = 0;
 for i = 1:a
     for j = 1:b
-        x = mmean1-ip(i,j);
-        sqsum = sqsum + x*x; 
+        k=ip(i,j);
+        x = double(abs(mmean1-k));
+        sqsum = sqsum + x*x;
     end
 end
-sqsum = sqsum/(a*b);
-sqsum = sqrt(single(sqsum));
+fprintf('sqsum is %f\n',sqsum);
+sqsum = sqsum/(a*b-1);
+
+sqsum = sqrt(double(sqsum));
 % sqsum = nthroot(sqsum,2);
 % sqsum = sqsum^(1/2);
 fprintf('variance is %f\n',sqsum);
 T = a*sqsum + b*mmean1;
+T
 I = global_Thresholding_1(ip,T);        
 end
 
