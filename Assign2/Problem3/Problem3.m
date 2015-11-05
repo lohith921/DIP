3e999dd8037e6efc240736440ce37da0d57c30b6
lohith921 <nanuvala.lohith@gmail.com>
2015-11-04 19:27:10 -0600

    Combining all together

--- Assign2/Problem3/Problem3.m
+++ Assign2/Problem3/Problem3.m
@@ -1,7 +1,14 @@
 % matlab code for problem 3, segmentation through various methods.
 I = imread('building.jpg');
 I = rgb2gray(I);
-%lets define threshold
-I1=global_Thresholding(I,50);
+% lets threshold=100
+% I1=global_Thresholding(I,50);
+%I1 = global_Thresholding_1(I,100);
 figure, imshow(I); title('original image');
-figure, imshow(I1); title('Segmented image using global thresholding');
+%figure, imshow(I1); title('Segmented image using global thresholding');
+% end of global thresholding.
+% Otsu's method of segmentation
+level = graythresh(I);
+J1 = im2bw(I,level);
+figure, imshow(J1); title('Segmented image using Otsus method');
+%end
