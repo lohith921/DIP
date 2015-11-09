b5cd1937b5c8f66b5a35babf057c954b72a7b493
lohith921 <nanuvala.lohith@gmail.com>
2015-11-09 00:14:59 -0600

    full program for building and object images

--- Assign2/Problem3/Problem3.m
+++ Assign2/Problem3/Problem3.m
@@ -1,14 +1,30 @@
 % matlab code for problem 3, segmentation through various methods.
+% read all the input files
 I = imread('building.jpg');
-I = rgb2gray(I);
-% lets threshold=100
-% I1=global_Thresholding(I,50);
-%I1 = global_Thresholding_1(I,100);
-figure, imshow(I); title('original image');
-%figure, imshow(I1); title('Segmented image using global thresholding');
+J = imread('object.jpg');
+Z = imread('indoor.jpeg');
+% converting all to gray
+I1 = rgb2gray(I);
+J1 = rgb2gray(J);
+Z1 = rgb2gray(Z);
+figure, imshow(I); title('Building image');
+% global Thresholding with threshold=100
+I2 = global_Thresholding(I1,100);
+figure, imshow(I2); title('Building segmented using global thresholding');
+J2 = global_Thresholding(J1,100);
+figure, imshow(J2); title('Object segmented using global thresholding');
+Z2 = global_Thresholding(Z1,100);
+figure, imshow(Z2); title('Indoor segmented using global thresholding');
 % end of global thresholding.
 % Otsu's method of segmentation
-level = graythresh(I);
-J1 = im2bw(I,level);
-figure, imshow(J1); title('Segmented image using Otsus method');
-%end
+level = graythresh(I1);
+I3 = im2bw(I1,level);
+figure, imshow(I3); title('Building segmented using Otsus method');
+level = graythresh(J1);
+J3 = im2bw(J1,level);
+figure, imshow(J3); title('Object segmented using Otsus method');
+level = graythresh(Z1);
+Z3 = im2bw(Z1,level);
+figure, imshow(Z3); title('Indoor segmented using Otsus method');
+% end of otsu's method
+% segmentation 
