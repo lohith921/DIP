3a7a50de6baff105b3f70e7d7753b6d6e4fc2006
lohith921 <nanuvala.lohith@gmail.com>
2015-11-09 00:12:26 -0600

    Program has been extended to include other images as well

--- Assign2/Problem1/Problem1.m
+++ Assign2/Problem1/Problem1.m
@@ -1,9 +1,38 @@
 clear all;
-I=imread('building.jpg');
-I1=rgb2gray(I);
-I2=edge(I1,'Roberts');
+% read the input image
+I = imread('building.jpg');
+J = imread('object.jpg');
+Z = imread('indoor.jpg');
+% converting to gray scale
+I1 = rgb2gray(I);
+J1 = rgb2gray(J);
+Z1 = rgb2gray(Z);
+% Roberts edge detection
+I2 = edge(I1,'Roberts');
 figure, imshow(I2); title('Building with Roberts edge detection');
-I2=edge(I1,'Sobel');
-figure, imshow(I2); title('Building with Sobel edge detection');
-I2=edge(I1,'Canny');
-figure, imshow(I2); title('Building with Canny edge detection');
+J2 = edge(J1,'Roberts');
+figure, imshow(J2); title('Object with Roberts edge detection');
+Z2 = edge(Z1,'Roberts');
+figure, imshow(Z2); title('Indoor with Roberts edge detection');
+% Sobel edge detection
+I3 = edge(I1,'Sobel');
+figure, imshow(I3); title('Building with Sobel edge detection');
+J3 = edge(J1,'Sobel');
+figure, imshow(J3); title('Object with Sobel edge detection');
+Z3=edge(Z1,'Sobel');
+figure, imshow(Z3); title('Indoor with Sobel edge detection');
+%Marr-Hildreth edge detection
+I4 = edge(I1,'log');
+figure, imshow(I4); title('Building with Marry-Hilderth edge detection');
+J4 = edge(J1,'log');
+figure, imshow(J4); title('Object with Marry-Hilderth edge detection');
+Z5 = edge(Z1,'log');
+figure, imshow(I5); title('Indoor with Marry-Hilderth edge detection');
+% Canny edge detection
+I5 = edge(I1,'Canny');
+figure, imshow(I5); title('Building with Canny edge detection');
+J5 = edge(J1,'Canny');
+figure, imshow(J5); title('Object with Canny edge detection');
+Z5 = edge(Z1,'Canny');
+figure, imshow(I4); title('Indoor with Canny edge detection');
+% end of program
