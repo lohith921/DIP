8fc6f814606fb2bb1b6c83483707d5797e1b6b67
lohith921 <nanuvala.lohith@gmail.com>
2015-11-09 21:56:14 -0600

    Program to plot the lines as well

---
+++ Assign2/Problem2/Problem2_3.m
@@ -0,0 +1,19 @@
+% function to plot the lines detected by hough transform
+clear all;
+% reading the images
+I = imread('building.jpg');
+J = imread('object.jpg');
+Z = imread('indoor.jpg');
+% convert to gray scale
+% b = 'building';
+% o = 'object';
+% i = 'indoor';
+I1 = rgb2gray(I);
+J1 = rgb2gray(J);
+Z1 = rgb2gray(Z);
+Hough_lines(I1);
+%
+Hough_lines(J1);
+% 
+Hough_lines(Z1);
+% end of program
