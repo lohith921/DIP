346a351a30f441caf174526c4ad0e12e70658549
lohith921 <nanuvala.lohith@gmail.com>
2015-11-09 21:06:36 -0600

    variable thresholding using local mean and variance

--- Assign2/Problem3/variable_thresholding.m
+++ Assign2/Problem3/variable_thresholding.m
@@ -1,19 +1,10 @@
-function [ Img ] = variable_thresholding( I )
-% variable_thresholding function does the thresholding based on local image
-% properties.
-[m,n] = size(I);
-Img = zeros(m,n);
-P1 = I(1:m/2,1:n/2);
-P2 = I(1:m/2,n/2+1:end);
-P3 = I(m/2+1:end,1:n/2);
-P4 = I(m/2+1:end,n/2+1:end);
-P1_1 = local_thresholding(P1);
-P2_1 = local_thresholding(P2);
-P3_1 = local_thresholding(P3);
-P4_1 = local_thresholding(P4);
-Img(1:m/2,1:n/2) = P1_1;
-Img(1:m/2,n/2+1:end) = P2_1;
-Img(m/2+1:end,1:n/2) = P3_1;
-Img(m/2+1:end,n/2+1:end) = P4_1;
+function [ Iout ] = variable_thresholding( In )
+% computing mean
+m = mean(mean(In));
+% local standard deviation of image
+variance = stdfilt(In,ones(5));
+%size(variance)
+Iout = (In > variance) & (In > m);
+% figure, imshow(I2); title('variable thresholding');
 end
 
