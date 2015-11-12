2525ced716a9ebefc155536f1af20c2dfd5106e8
lohith921 <nanuvala.lohith@gmail.com>
2015-11-12 08:59:40 -0600

    function calls are added with additional parameters needed

--- Project/Project_main.m
+++ Project/Project_main.m
@@ -1,13 +1,13 @@
 % This program reads an input image and calls different contrast enhancement methods
-Input = imread('');
+Input = imread('Uexposed.jpg');
 Input = rgb2gray(Input);
-figure, imshow(Input); title('Original Image');
-Iwthe = wthe(Input, 250, 10, 2);
-figure, imshow(Iwthe); title('Image enhanced with WTHE');
-Ibubo = bubo(Input);
+%figure, imshow(Input); title('Original Image');
+% Iwthe = wthe(Input, 250, 10, 2);
+% figure, imshow(Iwthe); title('Image enhanced with WTHE');
+Ibubo = bubo( Input,150 );
 figure, imshow(Ibubo); title('Image enhanced with BUBO');
-Ibbhe = bbhe(Input);
-figure, imshow(Ibbhe); title('Image enhanced with BBHE');
-Immbe = mmbebhe(Input);
-figure, imshow(Immbe); title('Image enhanced with MMBEBHE');
+% Ibbhe = bbhe(Input);
+% figure, imshow(Ibbhe); title('Image enhanced with BBHE');
+% Immbe = mmbebhe(Input);
+% figure, imshow(Immbe); title('Image enhanced with MMBEBHE');
 % end
