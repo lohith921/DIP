1983d1e879e169422af42532d4f4d895f51d10a8
lohith921 <nanuvala.lohith@gmail.com>
2015-11-12 08:58:29 -0600

    program has been modified to run for an input image

--- Project/wthe.m
+++ Project/wthe.m
@@ -1,46 +1,52 @@
-function [ out ] = wthe(In, up, low, r)
+function [ out ] = wthe(In, r)
 % Weighted thresholded intensity transformation program
 % Paper: Fast Image/Video Contrast Enhancement Based on Weighted
-% Thresholded Histogram Equalization 
+% Thresholded Histogram Equalization
 [m,n] = size(In);
- K = 256;
- p[K];
- for i=1:K
+sz = m*n;
+K = max(max(In));
+pwt = zeros(K,1);
+p = zeros(K,1);
+for i=1:K
     temp = find(In==(i-1));
-	nk = size(temp);
-	p(i) = nk/m*n;
- end
- % weighted probability function
- pwt[K]; 
- for i = 1:K
+    [nk, d] = size(temp);
+    p(i) = nk/sz;
+end
+% weighted probability function
+p1 = sort(p);
+% selecting probabilities from top 10 and bottow 10 for Pu and Pl
+up = p1(K-10,:);
+low = p1(10,:);
+for i = 1:K
     if( p(i)>up)
-	  pwt(i)=up;
-	  elseif( p(i)<low)
-	  pwt(i)=0;
-	  else
-	  t = (p(i)-low)/(up-low);
-	  t1 = pow(t,r)*up;
-	  pwt(i)=t1;
-	end
- end
- % cumulative distribution function
- Cwt[K];
- for i=1:K
-  for j=1:i
-    su = su+pwt(j);
-  end
-  Cwt(i)=su;
- end
- Inew = In;
- % temporary parameter values
- Wout = K-1;
- Madj = 0;
- for i=1:m
-  for j=1:n
-   Inew(i,j)=Wout*Cwt[In(i,j)]+Madj;
-  end
- end
- out = Inew
- end
- % end of program
+        pwt(i)=up;
+    elseif( p(i)<low)
+        pwt(i)=0;
+    else
+        t = (p(i)-low)/(up-low);
+        t1 = (t^r)*up;
+        pwt(i)=t1;
+    end
+end
+% cumulative distribution function
+Cwt = size(K,1);
+su = 0;
+for i=1:K
+    for j=1:i
+        su = su + pwt(j);
+    end
+    Cwt(i)=su;
+end
+Inew = In;
+% temporary parameter values
+Wout = K-1;
+Madj = 0;
+for i=1:m
+    for j=1:n
+        Inew(i,j)=Wout*Cwt(In(i,j)+1)+Madj;
+    end
+end
+out = Inew;
+end
+% end of program
 
