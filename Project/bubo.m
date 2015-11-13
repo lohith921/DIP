fceaa6e8c1d7608ae220bc640908c4992873426f
lohith921 <nanuvala.lohith@gmail.com>
2015-11-12 19:37:59 -0600

    Program runs but o/p is not as expected.

--- Project/bubo.m
+++ Project/bubo.m
@@ -1,12 +1,15 @@
 function [out] = bubo(In, alpha)
 % program for contrast enhancement using bin underflow and bin overflow mechanism
-% The corresponding paper is CONTRAST ENHANCEMENT USING HISTOGRAM EQUALIZATION
-% WITH BIN UNDERFLOWAND BIN OVERFLOW
+% The corresponding paper is 'CONTRAST ENHANCEMENT USING HISTOGRAM EQUALIZATION
+% WITH BIN UNDERFLOWAND BIN OVERFLOW'
+In = imresize(In, 0.65);
+figure, imshow(In); title('Reduced image');
 [M,N]=size(In);
 sz = M*N;
-Cbu = (1-alpha)/sz;
-Cbo = (1+alpha)/sz;
-K = 256;
+Cbu = (1 - alpha)/sz;
+Cbo = (1 + alpha)/sz;
+% K = 256;
+K = max(max(In));
 p = zeros(K,1); % probability density function
 for i=1:K
     temp = find(In==(i-1));
@@ -30,13 +33,14 @@ for i=1:K
     for j=1:i
         su = su+pn(j);
     end
-    C(i)=su;
+    C(i) = su;
+    su = 0;
 end
 Iout = In;
 for i=1:M
     for j=1:N
         k = In(i,j);
-        t= (C(K)/K)*K;
+        t= (C(K)/K)*k;
         t1 = K*(C(k+1)-t)+k;
         Iout(i,j) = t1;
     end
