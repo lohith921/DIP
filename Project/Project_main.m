abab91bc95e3d9ac3b0921556e2a6684ed00cfa9
lohith921 <nanuvala.lohith@gmail.com>
2015-11-16 15:34:50 -0600

    casual changes

--- Project/Project_main.m
+++ Project/Project_main.m
@@ -2,10 +2,13 @@
 Input = imread('Uexposed.jpg');
 Input = rgb2gray(Input);
 %figure, imshow(Input); title('Original Image');
+Iout = bbhe(Input);
+figure, imshow(Iout); title('Output image');
+figure, imhist(Iout);
 % Iwthe = wthe(Input, 250, 10, 2);
 % figure, imshow(Iwthe); title('Image enhanced with WTHE');
-Ibubo = bubo( Input,150 );
-figure, imshow(Ibubo); title('Image enhanced with BUBO');
+%Ibubo = bubo( Input,150 );
+%figure, imshow(Ibubo); title('Image enhanced with BUBO');
 % Ibbhe = bbhe(Input);
 % figure, imshow(Ibbhe); title('Image enhanced with BBHE');
 % Immbe = mmbebhe(Input);
