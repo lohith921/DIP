c9bde6c5a1a1214406962deaecc7b430c753cc97
lohith921 <nanuvala.lohith@gmail.com>
2015-11-16 15:33:45 -0600

    modified the code to be close the algorithm

--- Project/bbhe.m
+++ Project/bbhe.m
@@ -2,57 +2,80 @@ function [out]= bbhe(In)
 % program for brightness preserving bi-histogram equalization
 % corresponding paper is:
 [m,n]=size(In);
-K = max(max(In));
+% K = int16(max(max(In)));
+L = int16(256);
 % K=256;
-me = mean2(In);
-me = round(me);
+me = int16(mean2(In));
+% me = round(me);
 % dividing into sub images
-Ilow = (In<=me);
+Ilow = zeros(100, 1);
+Ihigh = zeros(100, 1);
+Ii = 1;
+Ih = 1;
+for i=1:m
+    for j=1:n
+        if(In(i,j) <= me)
+            Ilow(Ii) = In(i,j);
+            Ii = Ii + 1;
+        else
+            Ihigh(Ih) = In(i,j);
+            Ih = Ih + 1;
+        end
+    end
+end
+% Ilow = find(In<=me);
 [mlow, nlow] = size(Ilow);
 Nlow = mlow*nlow;
-Ihigh = (In>me);
+% Ihigh = find(In>me);
 [mhigh, nhigh] = size(Ihigh);
 Nhigh = mhigh*nhigh;
 % probabilities
 plow = zeros(me,1);
-phigh = zeros(K-me+1,1);
+t = L-me;
+phigh = zeros(t,1);
 % compute probability density functions for both sub images
-for i=1:me
+for i = 1:me
     temp = find(Ilow==(i-1));
     [nk,~] = size(temp);
     plow(i) = nk/Nlow;
 end
-for i=me+1:K
+j=1;
+for i = me+1:L
     temp = find(Ihigh==(i-1));
     [nk,~] = size(temp);
-    phigh(i) = nk/Nhigh;
+    phigh(j) = nk/Nhigh;
+    j = j+1;
 end
 % lets compute corresponding cumulative distribution function
 Clow = zeros(me,1);
-Chigh = zeros(K-1-me,1);
+t1 = L-me;
+Chigh = zeros(t1,1);
 su = 0;
-for i=1:me
-    for j=1:i
+for i = 1:me
+    for j = 1:i
         su = su+plow(j);
     end
-    Clow(i)=su;
+    Clow(i) = su;
     su = 0;
 end
-for i=me+1:K
-    for j=1:i
+j = int16(1);
+for i = me+1:L-1
+    for k = me+1:i
         su = su+phigh(j);
     end
-    Chigh(i)=su;
+    Chigh(j) = su;
+    j = j+1;
     su = 0;
 end
 % output intensity mapping function
 Iout = In;
-for i=1:m
-    for j=1:n
-        if(In(i,j)<me)
-            Iout(i,j) = (me-In(i,j))*Clow(In(i,j)+1);
+for i = 1:m
+    for j = 1:n
+        p = int16(In(i,j));
+        if(p < me)
+            Iout(i,j) = (me-p)*Clow(p+1);
         else
-            Iout(i,j) = me+1 + (K-In(i,j))*Chigh(In(i,j)+1);
+            Iout(i,j) = me + (L-p)*Chigh(L-p);
         end
     end
 end
