6241aa03471b3267f3f4f3fc4cd982b1434a6642
lohith921 <nanuvala.lohith@gmail.com>
2015-11-16 15:34:32 -0600

    removed the % selection of probability and used the code in paper

--- Project/wthe.m
+++ Project/wthe.m
@@ -1,41 +1,45 @@
-function [ out ] = wthe(In, r, alpha)
+function [ out ] = wthe(In, r)
 % Weighted thresholded intensity transformation program
 % Paper: Fast Image/Video Contrast Enhancement Based on Weighted
 % Thresholded Histogram Equalization
+v = 0.5;
 [m,n] = size(In);
 sz = m*n;
-K = max(max(In));
+% K = max(max(In));
+K = 256;
 pwt = zeros(K,1);
 p = zeros(K,1);
-for i=1:K
+for i = 1:K
     temp = find(In==(i-1));
     [nk, d] = size(temp);
     p(i) = nk/sz;
 end
-% weighted probability function
-p1 = sort(p);
-% selecting probabilities from top 10 and bottow 10 for Pu and Pl
-up = p1(K-alpha,:);
-low = p1(alpha,:);
+% % weighted probability function
+% p1 = sort(p);
+% % selecting probabilities from top 10 and bottow 10 for Pu and Pl
+% up = p1(K-alpha,:);
+% low = p1(alpha,:);
+Pu = v*max(p);
+Pl = min(p);
 for i = 1:K
-    if( p(i)>up)
-        pwt(i)=up;
-    elseif( p(i)<low)
-        pwt(i)=0;
+    if(p(i) > Pu)
+        pwt(i) = Pu;
+    elseif(p(i) <= Pl)
+        pwt(i) = 0;
     else
-        t = (p(i)-low)/(up-low);
-        t1 = (t^r)*up;
-        pwt(i)=t1;
+        t = (p(i)-Pl)/(Pu-Pl);
+        t1 = (t^r)*Pu;
+        pwt(i) = t1;
     end
 end
 % cumulative distribution function
 Cwt = size(K,1);
 su = 0;
-for i=1:K
-    for j=1:i
+for i = 1:K
+    for j = 1:i
         su = su + pwt(j);
     end
-    Cwt(i)=su;
+    Cwt(i) = su;
 end
 Inew = In;
 % temporary parameter values
@@ -43,7 +47,7 @@ Wout = K-1;
 Madj = 0;
 for i=1:m
     for j=1:n
-        Inew(i,j)=Wout*Cwt(In(i,j)+1)+Madj;
+        Inew(i,j) = Wout*Cwt(In(i,j)+1)+Madj;
     end
 end
 out = Inew;
