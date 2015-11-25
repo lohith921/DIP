b36e397f65e957db0fd19fc6be67f0b295bbe917
lohith921 <nanuvala.lohith@gmail.com>
2015-11-25 12:37:46 -0600

    performs the gauss filteration by array multiplication

--- Assign3/Problem 1/gauss_lowpass.m
+++ Assign3/Problem 1/gauss_lowpass.m
@@ -1,12 +1,16 @@
-function [out] = gauss_lowpass(In, D0)
+function [out] = gauss_lowpass(In)
 % function to implement the gaussian low pass filter.
 [P , Q] = size(In);
-out = In;
+D0 = sum(sum(In))/(P*Q);
+D0
+% out = In;
+H = zeros(P,Q);
 for i = 1:P
     for j = 1:Q
         D = (i-P/2)^2 + (j-Q/2)^2;
         D = sqrt(D);
-        out(i,j) = exp((-D)^2 / 2*(D0^2));
+        H(i,j) = exp((-1)*(D^2) / 2*(D0^2));
     end
 end
+out = H.*In;
 end
