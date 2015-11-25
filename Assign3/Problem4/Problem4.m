08ce220820180b7b5006309cbf73afe2e96fd942
lohith921 <nanuvala.lohith@gmail.com>
2015-11-25 17:57:01 -0600

    main code for problem 4

---
+++ Assign3/Problem4/Problem4.m
@@ -0,0 +1,69 @@
+clear;
+close all;
+I = imread('flower1.jpg');
+I = im2double(I);
+% J = imread('flower2,jpg');
+[M,N,p]=size(I);
+if p ~= 0
+    I = rgb2gray(I);
+end
+if M>N
+    P = M;
+else
+    P = N;
+end
+figure, imshow(I); title('original image');
+% I = imnoise(I,'gaussian',0,0.5);
+% figure, imshow(I); title('with gaussian noise');
+H = zeros(P,P);
+k = 0.002;
+for u = 1:P
+    for v = 1:P
+        l = (u^2 + v^2)^(5/6);
+        l = -1*k*l;
+        H(u,v) = exp(l);
+    end
+end
+F = zeros(P,P);
+F(1:M, 1:N) = I;
+% F = real(fft2(F));
+F = fft2(F);
+% adding atmospheric turbulence
+G = F.*H;
+figure, imshow(real(ifft2(G))); title('image with atmospheric turbulence');
+for i = 1:P
+    for j = 1:P
+        G(i,j) = (-1)^(i+j)*G(i,j);
+    end
+end
+% lets add gaussian noise with mean = 0 and variance 5.
+Gn = imnoise(G,'gaussian',0,0.5);
+figure, imshow(real(Gn)); title('image with gaussian noise');
+% lets apply inverse filtering. 
+% designing filter with width 70
+for u = 1:P
+    for v = 1:P
+        l = ((u)^2 + (v)^2)^(5/6);
+        l = -1*k*l;
+        H(u,v) = exp(l);
+    end
+end
+% Noise
+N1 = Gn - G;
+G = G./H;
+figure, imshow(real(G));title('figure with divided H');
+%N = Gn./H;
+N1 = N1./H;
+F = G - N1;
+Iout = real(ifft2(F));
+% for i = 1:P
+%     for j = 1:P
+%         Iout(i,j) = (-1)^(i+j)*Iout(i,j);
+%     end
+% end
+figure, imshow(Iout(1:M,1:N)); title('image restored using inverse filtering');
+% end
+
+
+
+
