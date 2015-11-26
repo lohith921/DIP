28251059959971ec5a068d5aeb77ebe9f2e337a5
lohith921 <nanuvala.lohith@gmail.com>
2015-11-25 19:09:34 -0600

    program is taking long time

--- Assign3/Problem2/Problem2.m
+++ Assign3/Problem2/Problem2.m
@@ -2,7 +2,7 @@ clear;
 close all;
 I = imread('moonlanding.png');
 I = im2double(I);
-figure, imshow(I); title('original image');
+%figure, imshow(I); title('original image');
 [M,N] = size(I);
 % padding parameters
 P = 2*M;
@@ -12,20 +12,20 @@ Fp = zeros(P,Q);
 % copying image to the top left quadrant
 Fp(1:M,1:N) = I;
 % multiplying with -1^(u+v) to center the transform.
-for i = 1:P
-    for j = 1:Q
-        Fp(i,j) = power(-1,(i+j))*Fp(i,j);
-    end
-end
+% for i = 1:P
+%     for j = 1:Q
+%         Fp(i,j) = power(-1,(i+j))*Fp(i,j);
+%     end
+% end
 % Fourier transform of the image.
+ Ftr = real(fft2(Fp));
 % Ftr = real(fft2(Fp));
-Ftr = fft2(Fp);
 % figure, imshow(Itr); title('spectrum');
 % lets design the butterworth band reject filter
 H = zeros(P,Q);
-n = 2; % order of butterworth filter
-D0 = 300;
-W = 100; % width of the band
+n = 0.2; % order of butterworth filter
+D0 = 70;
+W = 150; % width of the band
 a = P/2;
 b = Q/2;
 for u = 1:P
@@ -39,11 +39,11 @@ for u = 1:P
 end
 % lets construct band pass filter
 Hp = zeros(P,Q);
-for i = 1:P
-    for j = 1:Q
-        Hp(i,j) = 1-H(i,j);
-    end
-end        
+% for i = 1:P
+%     for j = 1:Q
+%         Hp(i,j) = 1-H(i,j);
+%     end
+% end        
 G = H.*Ftr;
 Fp = real(ifft2(G));
 % for i = 1:P
@@ -52,6 +52,6 @@ Fp = real(ifft2(G));
 %     end
 % end
 Iout = Fp(1:M,1:N);
-figure, imshow(Iout); title('Filtered with n=2,D0=300,W=100');
+figure, imshow(Iout); title('Filtered with n=0.2,D0=70,W=150');
 % end
 
