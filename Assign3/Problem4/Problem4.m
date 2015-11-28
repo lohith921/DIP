218efe31fa1d506e70d8e6d592ab9610a91ede64
lohith921 <nanuvala.lohith@gmail.com>
2015-11-27 20:48:06 -0600

    tried some modifications, but didnot work

--- Assign3/Problem4/Problem4.m
+++ Assign3/Problem4/Problem4.m
@@ -1,30 +1,33 @@
 clear;
 close all;
-I = imread('flower1.jpg');
+I = imread('flower1.1.jpg');
 I = im2double(I);
 % J = imread('flower2,jpg');
 [M,N,p]=size(I);
 if p ~= 0
     I = rgb2gray(I);
 end
-if M>N
-    P = M;
-else
-    P = N;
-end
+% if M>N
+%     P = M;
+% else
+%     P = N;
+% end
+P = 2*M;
+Q = 2*N;
 figure, imshow(I); title('original image');
 % I = imnoise(I,'gaussian',0,0.5);
 % figure, imshow(I); title('with gaussian noise');
-H = zeros(P,P);
+H = zeros(P,Q);
 k = 0.002;
+% computing convolution matrix for atmospheric turbulence
 for u = 1:P
-    for v = 1:P
+    for v = 1:Q
         l = (u^2 + v^2)^(5/6);
         l = -1*k*l;
         H(u,v) = exp(l);
     end
 end
-F = zeros(P,P);
+F = zeros(P,Q);
 F(1:M, 1:N) = I;
 % F = real(fft2(F));
 F = fft2(F);
@@ -32,10 +35,11 @@ F = fft2(F);
 G = F.*H;
 figure, imshow(real(ifft2(G))); title('image with atmospheric turbulence');
 for i = 1:P
-    for j = 1:P
+    for j = 1:Q
         G(i,j) = (-1)^(i+j)*G(i,j);
     end
 end
+
 % lets add gaussian noise with mean = 0 and variance 5.
 Gn = imnoise(G,'gaussian',0,0.5);
 figure, imshow(real(Gn)); title('image with gaussian noise');
@@ -43,7 +47,7 @@ figure, imshow(real(Gn)); title('image with gaussian noise');
 % designing filter with width 70
 for u = 1:P
     for v = 1:P
-        l = ((u)^2 + (v)^2)^(5/6);
+        l = (u^2 + v^2)^(5/6);
         l = -1*k*l;
         H(u,v) = exp(l);
     end
