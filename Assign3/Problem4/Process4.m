17d3a0cb18130c7c82feb631fd8971b22af0ec8f
lohith921 <nanuvala.lohith@gmail.com>
2015-11-29 12:21:21 -0600

    Final version of processing function

--- Assign3/Problem4/Process4.m
+++ Assign3/Problem4/Process4.m
@@ -1,83 +1,52 @@
 function [ ] = Process4( In )
 % Process4 does all the processing
-[M,N,p]=size(In);
-if p ~= 0
-    In = rgb2gray(In);
-end
-P = 2*M;
-Q = 2*N;
+In = rgb2gray(In);
+In = im2double(In);
+[M,N]=size(In);
 figure, imshow(In); title('Original image');
-% Padding
-Fp = zeros(P,Q);
-% copying image to top left quadrant
-Fp(1:M, 1:N) = In;
-% shifting
-for i = 1:P
-    for j = 1:Q
-        Fp(i,j) = power(-1,(i+j))*Fp(i,j);
-    end
-end
-% fourier transform
-Ftr = fft2(Fp);
+Ft = fftshift(fft2(In));
 % Atmospheric turbulence with k = 0.002
-H = zeros(P,Q);
 k = 0.002;
-% computing convolution matrix for atmospheric turbulence
-a = P/2;
-b = Q/2;
-for u = 1:P
-    for v = 1:Q
-        l = ((u-a)^2 + (v-b)^2)^(5/6);
-        l = -1*k*l;
-        H(u,v) = exp(l);
-    end
-end
-Ftr = H.*Ftr;
-Fr = real(ifft2(Ftr));
-% shifting
-for i = 1:P
-    for j = 1:Q
-        Fr(i,j) = power(-1,(i+j))*Fr(i,j);
-    end
-end
-figure, imshow(Fr(1:M,1:N)); title('Image with atmospheric turbulence');
+u0 = M/2;
+v0 = N/2;
+u = (1:M)-u0;
+v = (1:N)-v0;
+[U,V] = meshgrid(u,v);
+D = (U.^2+V.^2);
+H = exp(-k*(D.^(5/6)));
+Ftr = Ft .* H;
+Fblurred = real(ifft2(ifftshift(Ftr)));
+figure, imshow(Fblurred); title('Image with atmospheric turbulence');
 % lets add gaussian noise with mean = 0 and variance 0.02
-Gn = imnoise(Fr,'gaussian',0,0.02);
-figure, imshow(Gn(1:M,1:N)); title('Image with gaussian noise');
-% Inverse filtering begins now.
-% remove gaussian noise using mean filter
-% Fclear = conv2(single(Gn), ones(5)/25);
-% Fclear = imgaussfilt(Gn, 0.02);
-% Fclear = medfilt2(Gn,[9 9]);
-% % Fclear = gauss_lowpass(Gn);
-% figure, imshow(Fclear(1:M,1:N)); title('Gaussian noise removed');
-% lets transform it to frequency domain
-% fourier transform
-% Hr = H;
-% Hr(degradation function) is going to be the inverse of H. 
-% so when multiplied by Hr, the improper image should return original
-% image.
+Gn = imnoise(Fblurred,'gaussian',0,0.02);
+figure, imshow(Gn); title('Image with gaussian noise');
 % designing filter with width 70
-Ftclear = real(fftshift(fft2(Fr)));
+Fblurrnoisy = fftshift(fft2(Gn));
+Fblurr = gauss_lowpass(Fblurrnoisy);
+figure, imshow(real(ifft2(ifftshift(Fblurr)))); title('Gaussian noise removed');
 radius = 70;
-for i = 1:P
-    for j = 1:Q
+a = floor(M/2)+1;
+b = floor(N/2)+1;
+for i = 1:M
+    for j = 1:N
         dist = ((i-a)^2 + (j-b)^2)^0.5;
         if dist < radius
-            Ftclear(i,j) = Ftclear(i,j)/H(i,j);
+            Fblurr(i,j) = Fblurr(i,j)/H(i,j);
         else
-            Ftclear(i,j) = 0;
+            Fblurr(i,j) = 0;
         end
     end
 end
-Fpreal = real(ifftshift(ifft2(Ftclear)));
-figure, imshow(Fpreal(1:M,1:N)); title('restored with inverse filter');
+% restoring
+Frreal = abs(ifft2(ifftshift(Fblurr)));
+figure, imshow(Frreal); title('restored with inverse filter');
 % weiner filter
 % W(i,j) = (1/H(i,j))*((abs(H(i,j))^2)/(abs(H(i,j))^2+1000));
-L=conj(H)./(H.*conj(H)+1000);
-Fpweiner= Fpreal.*L;
-figure, imshow(abs(ifft2(ifftshift(Fpweiner(1:M,1:N))))); title('Restored with Weiner filter');
-% Ffinal = Fpweiner(1:M,1:N);
-% figure, imshow(Ffinal); title('Final output image');
+L = conj(H)./(H .* (conj(H) + 1000));
+% H1 = abs(H).^2;
+% H2 = H1 + 1000;
+% H3 = H1./H2;
+% L = conj(H) * H3;
+Fweiner = L .* Fblurr;
+figure, imshow(abs(ifft2(ifftshift(Fweiner)))); title('Restored with Weiner filter');
 end
-
