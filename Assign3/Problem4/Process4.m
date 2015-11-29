3523ec3b3643ce4f09e2bbbbbed38b0e6f031184
lohith921 <nanuvala.lohith@gmail.com>
2015-11-29 10:23:19 -0600

    made some more changes, but still doesnt work

--- Assign3/Problem4/Process4.m
+++ Assign3/Problem4/Process4.m
@@ -14,12 +14,12 @@ Fp(1:M, 1:N) = In;
 % shifting
 for i = 1:P
     for j = 1:Q
-        Fp(i,j) = (-1)^(i+j)*Fp(i,j);
+        Fp(i,j) = power(-1,(i+j))*Fp(i,j);
     end
 end
 % fourier transform
 Ftr = fft2(Fp);
-% Atmospheric turbulence
+% Atmospheric turbulence with k = 0.002
 H = zeros(P,Q);
 k = 0.002;
 % computing convolution matrix for atmospheric turbulence
@@ -34,10 +34,10 @@ for u = 1:P
 end
 Ftr = H.*Ftr;
 Fr = real(ifft2(Ftr));
- % shifting back
+% shifting
 for i = 1:P
     for j = 1:Q
-        Fr(i,j) = (-1)^(i+j)*Fr(i,j);
+        Fr(i,j) = power(-1,(i+j))*Fr(i,j);
     end
 end
 figure, imshow(Fr(1:M,1:N)); title('Image with atmospheric turbulence');
@@ -48,39 +48,36 @@ figure, imshow(Gn(1:M,1:N)); title('Image with gaussian noise');
 % remove gaussian noise using mean filter
 % Fclear = conv2(single(Gn), ones(5)/25);
 % Fclear = imgaussfilt(Gn, 0.02);
-Fclear = medfilt2(Gn,[9 9]);
-% Fclear = gauss_lowpass(Gn);
-figure, imshow(Fclear(1:M,1:N)); title('Gaussian noise removed');
+% Fclear = medfilt2(Gn,[9 9]);
+% % Fclear = gauss_lowpass(Gn);
+% figure, imshow(Fclear(1:M,1:N)); title('Gaussian noise removed');
 % lets transform it to frequency domain
-% shifting
-for i = 1:P
-    for j = 1:Q
-        Fclear(i,j) = (-1)^(i+j)*Fclear(i,j);
-    end
-end
 % fourier transform
-Ftclear = real(fft2(Fclear));
-Hr = H;
+% Hr = H;
 % Hr(degradation function) is going to be the inverse of H. 
 % so when multiplied by Hr, the improper image should return original
 % image.
 % designing filter with width 70
-for u = 1:P
-    for v = 1:Q
-        l = ((u-a)^2 + (v-b)^2)^(5/6);
-        l = 1*k*l; % exponent is made positive.
-        Hr(u,v) = exp(l);
-    end
-end
-% convolving padded image
-Fpr = Hr.*Ftclear;
-Fpreal = real(ifft2(Fpr));
+Ftclear = real(fftshift(fft2(Fr)));
+radius = 70;
 for i = 1:P
     for j = 1:Q
-        Fpreal(i,j) = power(-1,(i+j))*Fpreal(i,j);
+        dist = ((i-a)^2 + (j-b)^2)^0.5;
+        if dist < radius
+            Ftclear(i,j) = Ftclear(i,j)/H(i,j);
+        else
+            Ftclear(i,j) = 0;
+        end
     end
 end
-Ffinal = Fpreal(1:M,1:N);
-figure, imshow(Ffinal); title('Final output image');
+Fpreal = real(ifftshift(ifft2(Ftclear)));
+figure, imshow(Fpreal(1:M,1:N)); title('restored with inverse filter');
+% weiner filter
+% W(i,j) = (1/H(i,j))*((abs(H(i,j))^2)/(abs(H(i,j))^2+1000));
+L=conj(H)./(H.*conj(H)+1000);
+Fpweiner= Fpreal.*L;
+figure, imshow(abs(ifft2(ifftshift(Fpweiner(1:M,1:N))))); title('Restored with Weiner filter');
+% Ffinal = Fpweiner(1:M,1:N);
+% figure, imshow(Ffinal); title('Final output image');
 end
 
