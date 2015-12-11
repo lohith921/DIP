cd6c33ab40c459ae2053f236f6387f22854c2f6e
lohith921 <nanuvala.lohith@gmail.com>
2015-12-10 22:32:34 -0600

    modified to include subplot and histogram for each method

--- Project/Project_main.m
+++ Project/Project_main.m
@@ -1,16 +1,24 @@
 % This program reads an input image and calls different contrast enhancement methods
 Input = imread('Uexposed.jpg');
 Input = rgb2gray(Input);
-%figure, imshow(Input); title('Original Image');
-Iout = bbhe(Input);
-figure, imshow(Iout); title('Output image');
-figure, imhist(Iout);
-% Iwthe = wthe(Input, 250, 10, 2);
-% figure, imshow(Iwthe); title('Image enhanced with WTHE');
-%Ibubo = bubo( Input,150 );
-%figure, imshow(Ibubo); title('Image enhanced with BUBO');
+% subplot(2,2,1), imshow(Input); title('original');
+% subplot(2,2,3), imhist(Input); title('histogram before');
+% Iout = bbhe(Input);
+% subplot(2,2,2), imshow(Iout); title('bbhe');
+% subplot(2,2,4), imhist(Iout); title('histogram after');
+% end
+subplot(2,2,1), imshow(Input); title('original');
+subplot(2,2,3), imhist(Input); title('histogram before');
+Iout = wthe(Input,1.5);
+subplot(2,2,2), imshow(Iout); title('wthe with v = 0.8,r = 1.5');
+subplot(2,2,4), imhist(Iout); title('histogram after');
+% subplot(2,2,1), imshow(Input); title('original');
+% subplot(2,2,3), imhist(Input);
+% subplot(2,2,2), imshow(Ibubo); title('enhanced with BUBO');
+% subplot(2,2,4), imhist(Ibubo);
 % Ibbhe = bbhe(Input);
 % figure, imshow(Ibbhe); title('Image enhanced with BBHE');
+% mmbebhe is not working
 % Immbe = mmbebhe(Input);
 % figure, imshow(Immbe); title('Image enhanced with MMBEBHE');
 % end
