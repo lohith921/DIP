ceb300d9222be649757b4a0e2f52864f90a07614
lohith921 <nanuvala.lohith@gmail.com>
2015-12-15 18:35:18 -0600

    modified the code and the outcome is reasonable

--- Project/bbhe.m
+++ Project/bbhe.m
@@ -4,38 +4,34 @@ function [out]= bbhe(In)
 % equalization in contrast enhancement"
 [m,n] = size(In);
 % K = int16(max(max(In)));
+X0 = min(min(In));
 L = 256;
 % K=256;
 me = round(mean2(In));
-
-Ilow = find(In <= me);
-Ihigh = find(In > me);
-
-[mlow, nlow] = size(Ilow);
-Nlow = mlow*nlow;
+Ilow = find(In < me);
+[nlow, ~] = size(Ilow);
+Ihigh = find(In >= me);
+[nhigh, ~] = size(Ihigh);
 % Ihigh = find(In>me);
-[mhigh, nhigh] = size(Ihigh);
-Nhigh = mhigh*nhigh;
 % probabilities
 plow = zeros(me,1);
-t = L-me;
-phigh = zeros(t,1);
+phigh = zeros(L-me,1);
 % compute probability density functions for both sub images
-for i = 1:me+1
+for i = 1:me-1
     temp = find(In==(i-1));
     [nk,~] = size(temp);
-    plow(i) = nk/Nlow;
+    plow(i) = nk/nlow;
+    temp = 0;
 end
-j=1;
-for i = me+2:L
-    temp = find(In==(i-1));
+for j = me:256
+    temp = find(In==(j-1));
     [nk,~] = size(temp);
-    phigh(j) = nk/Nhigh;
-    j = j+1;
+    phigh(j-me+1) = nk/nhigh;
+    temp = 0;
 end
 % lets compute corresponding cumulative distribution function
 Clow = zeros(me,1);
-Chigh = zeros(t,1);
+Chigh = zeros(L-me,1);
 su = 0;
 for i = 1:me
     for j = 1:i
@@ -59,7 +55,7 @@ for i = 1:m
     for j = 1:n
         p = In(i,j);
         if(p < me)
-            Iout(i,j) = (me-p)*Clow(p+1);
+            Iout(i,j) = (me-p)*Clow(me-p+1);
         else
             Iout(i,j) = me + (L-1-p)*Chigh(255-p+1);
         end
