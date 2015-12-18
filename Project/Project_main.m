69612d58ce95c2a3ef43bcd51b656e99f0004c02
lohith921 <nanuvala.lohith@gmail.com>
2015-12-17 20:35:22 -0600

    Final version of main program with all function calls included

--- Project/Project_main.m
+++ Project/Project_main.m
@@ -3,27 +3,45 @@
 prompt = 'Please provide the name of image file';
 filename = input(prompt,'s');
 Input = imread(filename);
-Input = rgb2gray(Input);
-Input = imresize(Input, [320 240]);
+%Input = rgb2gray(Input);
+%Input = imresize(Input, [320 240]);
 % Lets try brightness preserving bihistogram equalization
-subplot(2,2,1), imshow(Input); title('original');
-subplot(2,2,3), imhist(Input); title('histogram before');
-Iout = bbhe(Input);
-subplot(2,2,2), imshow(Iout); title('bbhe');
-subplot(2,2,4), imhist(Iout); title('histogram after');
+% subplot(3,2,1), imshow(Input); title('Original');
+% subplot(3,2,2), imhist(Input); title('Histogram');
+% Iout = bbhe(Input);
+% subplot(3,2,3), imshow(Iout); title('BBHE');
+% subplot(3,2,4), imhist(Iout); title('Histogram');
+% I = histeq(Input);
+% subplot(3,2,5), imshow(I); title('HE');
+% subplot(3,2,6), imhist(I); title('Histogram');
 % end
-%subplot(2,2,1), imshow(Input); title('original');
-% subplot(2,2,3), imhist(Input); title('histogram before');
-% Iout = wthe(Input,1.5);
-% subplot(2,2,2), imshow(Iout); title('wthe with v = 0.8,r = 1.5');
-% subplot(2,2,4), imhist(Iout); title('histogram after');
-% subplot(2,2,1), imshow(Input); title('original');
-% subplot(2,2,3), imhist(Input);
-% subplot(2,2,2), imshow(Ibubo); title('enhanced with BUBO');
-% subplot(2,2,4), imhist(Ibubo);
-% Ibbhe = bbhe(Input);
-% figure, imshow(Ibbhe); title('Image enhanced with BBHE');
-% mmbebhe is not working
-% Immbe = mmbebhe(Input);
-% figure, imshow(Immbe); title('Image enhanced with MMBEBHE');
+% subplot(3,2,1), imshow(Input); title('Original');
+% subplot(3,2,2), imhist(Input); title('Histogram');
+% Iout = dsihe(Input,240);
+% subplot(3,2,3), imshow(Iout); title('DSIHE');
+% subplot(3,2,4), imhist(Iout); title('Histogram');
+% I = histeq(Input);
+% subplot(3,2,5), imshow(I); title('HE');
+% subplot(3,2,6), imhist(I); title('Histogram');
+% end
+% subplot(3,2,1), imshow(Input); title('Original');
+% subplot(3,2,2), imhist(Input); title('Histogram');
+% Iout = bubo(Input,30);
+% subplot(3,2,3), imshow(Iout); title('BUBO');
+% subplot(3,2,4), imhist(Iout); title('Histogram');
+% I = histeq(Input);
+% subplot(3,2,5), imshow(I); title('HE');
+% subplot(3,2,6), imhist(I); title('Histogram');
+% end
+subplot(3,2,1), imshow(Input); title('Original');
+subplot(3,2,2), imhist(Input); title('Histogram');
+% Iout = wthe(<brain>,0.875);
+% Iout = wthe(<Oexposed>,0.75);
+% Iout = wthe(<hare>,0.875);
+Iout = wthe(Input,0.5);
+subplot(3,2,3), imshow(Iout); title('WTHE');
+subplot(3,2,4), imhist(Iout); title('Histogram');
+I = histeq(Input);
+subplot(3,2,5), imshow(I); title('HE');
+subplot(3,2,6), imhist(I); title('Histogram');
 % end
